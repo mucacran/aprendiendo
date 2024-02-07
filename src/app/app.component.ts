accf811ca9cd700358054e7a8be7c78896ac5265
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-06 23:09:05 -0500

    nuevos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Aprendiendo mucho hoy';
+  title = 'Aprendiendo mucho';
 
 
 }
