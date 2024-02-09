6a3e9cc0dc70d9e955b7376f70352d35f69a1860
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-08 20:21:24 -0500

    nuevos cambios
    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SonidoComponent } from './sonido/sonido.component';
-import { SaludoComponent } from './saludo/saludo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SonidoComponent,
-    SaludoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
