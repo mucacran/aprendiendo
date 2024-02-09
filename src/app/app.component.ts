35727440244db967329dfacf4aa6b26bfa378211
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-09 17:02:01 -0500

    nuevos ejemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  libros: Array<Object> = [];
+
+
   title = 'Aprendiendo mucho';
   valor1: boolean = false;
   valor2!: boolean;
 
   DiadelaSemana!: String; // Definir la propiedad DiadelaSemana como un número
   coolor!:string;
+
+  constructor(){
+    this.libros = [
+      {id:1, titulo:"el señor de los aniños",autor: "Sergio bravo"},
+      {id:2, titulo:"el señor de los aniños",autor: "Sergio bravo"},
+      {id:3, titulo:"el señor de los aniños",autor: "Sergio bravo"},
+      {id:4, titulo:"el señor de los aniños",autor: "Sergio bravo"}
+      /*
+      "uno",
+      "dos",
+      "tres"*/
+    ]
+  }
 }
