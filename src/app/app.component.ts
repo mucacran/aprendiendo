23665ce2bd8ccad36539ed57d326ac15c4c44870
mucacran <el.mucacran.rasta@hotmail.com>
2024-02-10 15:05:17 -0500

    hasta aqui aprendi sobre for y las propiedades de for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 
+
+
+interface Libro {
+  id: number;
+  titulo: string;
+  autor: string;
+}
+//los componentes o decoradores siempre tendran
+//que estar arriba de la clase exort
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
 
-  libros: Array<Object> = [];
+
+
+export class AppComponent {
+//esto no funciono pero luego abajo lo arregle
+  //libros: Array<Object> = [];
+
+  libros: Libro[] = [];
 
 
   title = 'Aprendiendo mucho';
@@ -19,14 +34,14 @@ export class AppComponent {
 
   constructor(){
     this.libros = [
-      {id:1, titulo:"el señor de los aniños",autor: "Sergio bravo"},
-      {id:2, titulo:"el señor de los aniños",autor: "Sergio bravo"},
-      {id:3, titulo:"el señor de los aniños",autor: "Sergio bravo"},
-      {id:4, titulo:"el señor de los aniños",autor: "Sergio bravo"}
-      /*
+      { id: 1, titulo: "El señor de los anillos", autor: "J.R.R. Tolkien" },
+      { id: 2, titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+      { id: 3, titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+      { id: 4, titulo: "1984", autor: "George Orwell" }
+/*
       "uno",
       "dos",
       "tres"*/
-    ]
+    ];
   }
 }
